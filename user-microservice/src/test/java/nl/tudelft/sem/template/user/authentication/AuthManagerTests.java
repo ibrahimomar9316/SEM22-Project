5c3bdc56e7ac58776914407f60c54f5a5de077cc
Wiktor Grzybko <w.j.grzybko@student.tudelft.nl>
2022-12-02 21:13:35 +0100

    resolving previous commit issues. only change "example" into "user" in some Strings

--- user-microservice/src/test/java/nl/tudelft/sem/template/user/authentication/AuthManagerTests.java
+++ user-microservice/src/test/java/nl/tudelft/sem/template/user/authentication/AuthManagerTests.java
@@ -1,8 +1,9 @@
-package nl.tudelft.sem.template.example.authentication;
+package nl.tudelft.sem.template.user.authentication;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
