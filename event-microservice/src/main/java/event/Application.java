542ca88bdff0f8ff38d1ed579df7f25cf6f43ac4
Cristian Soare <C.Soare@student.tudelft.nl>
2022-12-03 17:21:01 +0100

    Fixes Event DB name and adds Pipeline for event-microservice

--- event-microservice/src/main/java/event/Application.java
+++ event-microservice/src/main/java/event/Application.java
@@ -3,9 +3,6 @@ package event;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Example microservice application.
- */
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
