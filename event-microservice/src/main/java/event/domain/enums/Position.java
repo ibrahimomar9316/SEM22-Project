87ff05948cb30d80edc5e484d7fc5dc316e1a1bb
Glenn Weeland <G.S.M.Weeland@student.tudelft.nl>
2022-12-12 22:01:02 +0100

    Added participants and Positions for events
    - Added enum type: Position, which represents the position that has to be, or is already filled in for an event
    - Added embeddable class: Participant, which takes a netId (user) and a position to form a participant in an event. Participants can be created with a user, so they will be displayed in the event. Or without a user so the positions that need to be filled are clear.

---
+++ event-microservice/src/main/java/event/domain/enums/Position.java
@@ -0,0 +1,12 @@
+package event.domain.enums;
+
+/**
+ * Enum value representing the position for a participant in an event
+ */
+public enum Position {
+    COX,
+    COACH,
+    PORT_SIDE_ROWER,
+    STARBOARD_SIDE_ROWER,
+    SCULLING_ROWER
+}
