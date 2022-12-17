3fe2aded943ef5574c3d3e51238ce300769ea5e1
Cristian Soare <C.Soare@student.tudelft.nl>
2022-12-17 17:05:51 +0100

    Adds boilerplate implementation of Certificate Microservice (not working)

---
+++ certificate-microservice/src/main/java/certificate/CertificateMicroService.java
@@ -0,0 +1,14 @@
+package certificate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * App User microservice application.
+ */
+@SpringBootApplication
+public class CertificateMicroService {
+    public static void main(String[] args) {
+        SpringApplication.run(CertificateMicroService.class, args);
+    }
+}
