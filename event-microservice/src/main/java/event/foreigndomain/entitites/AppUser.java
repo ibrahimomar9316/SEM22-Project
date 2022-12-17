7800658f63959712d6c1f5a1421ade54e2ffeeb9
ibrahimomar <ibrhimomarabdallamohamed.student@tudelft.nl>
2022-12-17 22:52:56 +0100

    User preferences

--- event-microservice/src/main/java/event/foreigndomain/entitites/AppUser.java
+++ event-microservice/src/main/java/event/foreigndomain/entitites/AppUser.java
@@ -2,6 +2,9 @@ package event.foreigndomain.entitites;
 
 import event.foreigndomain.enums.Gender;
 
+import java.sql.Time;
+import java.util.Date;
+
 /**
  * The type App user.
  */
@@ -9,6 +12,10 @@ public class AppUser {
 
     private transient Gender gender;
     private transient String netId;
+    private transient String boat;
+    private transient Date avDate;
+    private transient Time avTime;
+
 
     /**
      * Instantiates a new App user.
@@ -26,11 +33,22 @@ public class AppUser {
      *
      * @param netId the net id
      */
-    public AppUser(String netId) {
+    public AppUser(String netId ) {
         this.netId = netId;
+
+    }
+    public void UserPref( String boat, Date date, Time time) {
+        this.avDate = date;
+        this.avTime = time;
+        this.boat = boat;
     }
 
     public String getNetId() {
         return netId;
     }
+    public String getBoat() { return boat;}
+    public Date getAvDate() {
+        return avDate;
+    }
+    public Time getAvTime() { return avTime;}
 }
--- user-microservice/src/main/java/nl/tudelft/sem/template/user/domain/entities/AppUser.java
+++ user-microservice/src/main/java/nl/tudelft/sem/template/user/domain/entities/AppUser.java
@@ -8,6 +8,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import nl.tudelft.sem.template.user.domain.enums.Gender;
 
+import java.util.Date;
+import java.sql.Time;
 /**
  * User entity consisting of an ID, gender, username, password, list of boats, list of certificates, list of roles.
  */
@@ -22,15 +24,59 @@ public class AppUser {
     @Id
     @Column(unique = true)
     private String netId;
+    private String boat;
+    private Date avdate;
+    private Time avtime;
 
     /**
      * Basic constructor for a new AppUser.
      *
      * @param netId a string containing the netId of the user so that we can differentiate them
+     *
      */
-    public AppUser(String netId) {
+    public AppUser(String netId ) {
         this.netId = netId;
+
     }
+/**
+ * Function to set User Preferences .
+ *
+ * @param boat a string containing the preferred boat type of the user
+ * @param date a date containing the available date of the user
+ * @param time a time containing the available time of the user
+ */
+    public void setUserPref( String boat, Date date, Time time) {
+    this.avdate = date;
+    this.avtime = time;
+    this.boat = boat;
+}
+    /**
+     * A getter returning the  available time of the user.
+     *
+     * @return the available time of the user
+     */
+    public Time getUserAvTime() {
+        return avtime;
+    }
+
+    /**
+     * A getter returning the  available date of the user.
+     *
+     * @return the available date of the user
+     */
+    public Date getUserAvDate() {
+        return avdate;
+    }
+    /**
+     * A getter returning the  preferred boat type of the user.
+     *
+     * @return the preferred boat type of the user
+     */
+    public String getPrefBoat() {
+        return boat;
+    }
+
+
 }
 
 
