353c9a5af49ebe3ff98d4bc439e17b02658b5df7
Radu-Stefan Ezaru <R.Ezaru@student.tudelft.nl>
2022-12-21 15:43:23 +0100

    Added queries to both  repos

--- certificate-microservice/src/main/java/certificate/domain/CertificateRepository.java
+++ certificate-microservice/src/main/java/certificate/domain/CertificateRepository.java
@@ -2,6 +2,7 @@ package certificate.domain;
 
 import certificate.domain.entities.Certificate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -9,5 +10,6 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface CertificateRepository extends JpaRepository<Certificate, Long> {
-
+    @Query("SELECT c FROM Certificate c WHERE c.netId = ?1")
+    Certificate getCertificateBy(String netId);
 }
--- certificate-microservice/src/main/java/certificate/domain/RuleRepository.java
+++ certificate-microservice/src/main/java/certificate/domain/RuleRepository.java
@@ -1,8 +1,10 @@
 package certificate.domain;
 
 
+import certificate.domain.entities.Certificate;
 import certificate.domain.entities.Rule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -10,5 +12,6 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface RuleRepository extends JpaRepository<Rule, Long> {
-
+    @Query("SELECT r FROM Rule r WHERE r.eventId = ?1")
+    Rule getRuleBy(long eventId);
 }
